66edb985121bf9444edb95f1cbc1c80d102dda0e
Vinicius <viniciusxp@Viniciuss-MacBook-Pro.local>
2017-12-21 05:32:56 +0300

    Exercise on Variables

---
+++ 03_Variables/01_shorthand.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var g string = "comwboy"
+
+func main() {
+
+	a := 10
+	b := "golanf"
+	c := 4.27
+	d := true
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+
+}
